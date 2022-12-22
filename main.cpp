8e0efc83bf7a6f66247a7e7b77c2731cdc2f7be4
Paolo-Castagno <117978458+Paolo-Castagno@users.noreply.github.com>
2022-12-22 09:34:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
+
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+float val (float x){
+    return (x*x)* cos(x)+1;
+}
+
 int main() {
-   return 0;
+    float a,b,x,err=1; 
+    
+    do{
+        cout<<"inserire estremi";
+        cin >> a >> b ;
+    } while (val(a)*val(b)>=0);
+
+    do {
+        x=(a+b)/2;
+        if (val (a)==0){
+            break;
+        } else {
+            if (val (a)*(b)<0){
+                b=x;
+            } else {
+                a=x;
+            }
+            err=fabs((b-a)/2);
+        
+        }
+    } while (err>=(1e-6));   
+        
+    x=x*10000;
+        x=(int)x;
+        cout <<x<<endl;
+        cout<< val (x);
 }
